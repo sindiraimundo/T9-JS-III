fd2d644a7e2f8a46dbab60f19e1015d78d6fe0f4
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-29 14:17:10 -0300

    resolução e explicação com resolução do exercicio main.j s utilizando fetch

--- 03-promises/exercicio/main.js
+++ 03-promises/exercicio/main.js
@@ -6,23 +6,46 @@ const localidade_input = document.getElementById("localidade")
 const uf_input = document.getElementById("uf")
 const cep_error = document.getElementById("cep-error")
 
-cep_input.addEventListener('change', () => {
-    console.log(cep_input.value)
+//***Resolução com get_data****
+// cep_input.addEventListener('change', () => {
+//     console.log(cep_input.value)
     
-    get_data(`https://viacep.com.br/ws/${cep_input.value}/json/`)
-        .then( (dadoJSON) => {
+//     get_data(`https://viacep.com.br/ws/${cep_input.value}/json/`)
+//         .then( (dadoJSON) => {
     
-            const dadoParseado = JSON.parse(dadoJSON)
-           
-            return dadoParseado
-        })
-        .then( (dadoObj) => {
-            logradouro_input.value = dadoObj.logradouro
-            bairro_input.value = dadoObj.bairro
-            localidade_input.value = dadoObj.localidade
-            uf_input.value = dadoObj.uf
-        })
-        .catch((erro) => {
-            console.log(erro)
-        })
+//             const dadoParseado = JSON.parse(dadoJSON)
+//            console.log(dadoJSON);
+//             return dadoParseado
+//         })
+//         .then( (dadoObj) => {
+//             logradouro_input.value = dadoObj.logradouro
+//             bairro_input.value = dadoObj.bairro
+//             localidade_input.value = dadoObj.localidade
+//             uf_input.value = dadoObj.uf
+//         })
+//         .catch((erro) => {
+//             console.log(erro)
+//         })
+// })
+
+//****resolvendo com o metodo FETCH****
+cep_input.addEventListener("blur", () => {
+    fetch(`https://viacep.com.br/ws/${cep_input.value}/json/`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        logradouro_input.value = data.logradouro;
+        bairro_input.value = data.bairro;
+        cep_error.textContent = ""
+    })
+    .catch(
+        (err) => { console.log('catch', err)
+    cep_error.textContent = "Errooou"
+    })
+});
+
+fetch(url) // O FETCH RETORNA UMA PROMISE
+.then(response => {return response.json()})
+.then(data => {
+    console.log('data', data)
 })
