a675d7376d2669e45a1d206678f966241cd1ff86
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-29 12:39:57 -0300

    resolução exercício Promise no main.js, utilizando get_data, then e catch e a API viacep

--- 03-promises/exercicio/main.js
+++ 03-promises/exercicio/main.js
@@ -5,3 +5,24 @@ const bairro_input = document.getElementById("bairro")
 const localidade_input = document.getElementById("localidade")
 const uf_input = document.getElementById("uf")
 const cep_error = document.getElementById("cep-error")
+
+cep_input.addEventListener('change', () => {
+    console.log(cep_input.value)
+    
+    get_data(`https://viacep.com.br/ws/${cep_input.value}/json/`)
+        .then( (dadoJSON) => {
+    
+            const dadoParseado = JSON.parse(dadoJSON)
+           
+            return dadoParseado
+        })
+        .then( (dadoObj) => {
+            logradouro_input.value = dadoObj.logradouro
+            bairro_input.value = dadoObj.bairro
+            localidade_input.value = dadoObj.localidade
+            uf_input.value = dadoObj.uf
+        })
+        .catch((erro) => {
+            console.log(erro)
+        })
+})
