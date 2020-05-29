53940a79c7f11cf81b4dd46f668294b8b3753f7e
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-29 16:19:01 -0300

    resolução do exemplo de fetch 04-fetch

--- 04-fetch/exemplo/main.js
+++ 04-fetch/exemplo/main.js
@@ -5,6 +5,22 @@ const bairroInput = document.getElementById("bairro");
 const localidadeInput = document.getElementById("localidade");
 const ufInput = document.getElementById("uf");
 
-cepInput.addEventListener("blur", () => {
 
+cepInput.addEventListener("blur", () => {
+    fetch(`https://viacep.com.br/ws/${cepInput.value}/json/`)
+        .then((resposta) => {
+            return resposta.json()
+        })
+        .then(dados => {
+            console.log(dados)
+            logradouroInput.value = dados.logradouro;
+            complementoInput.value = dados.complemento;
+            bairroInput.value = dados.bairro;
+            localidadeInput.value = dados.localidade;
+            ufInput.value = dados.uf;
+        })
 });
+
+
+ 
+ 
