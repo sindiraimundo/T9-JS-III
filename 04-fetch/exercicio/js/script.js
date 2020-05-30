f8e8d211c68e8339637ec2dd14bab836b26cedc0
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-05-30 14:41:07 -0300

    Resolvendo exercício de FETCH no evento de click, aparecer nova carta

--- 04-fetch/exercicio/js/script.js
+++ 04-fetch/exercicio/js/script.js
@@ -1,6 +1,7 @@
 const cardSection = document.getElementById('cards-section')
 const errorSection = document.getElementById('error')
 const loading = document.getElementById('spinner')
+const NovaCartaBtn = document.getElementById('chamarNovacartaBtn')
 // Caso os cards não carreguem, mostraremos isso:
 const placeholderCard = {
     nome: "Carta não carregada",
@@ -14,3 +15,22 @@ renderizaCarta = carta => {
     const novaCarta = new Card(carta).render()
     cardSection.innerHTML = novaCarta 
 }
+// Fazer requisição neste link:
+// https://github.com/reprograma/T9-JS-III/blob/sexta/04-fetch/exercicio/tarot.json
+// Renderizar uma carta aleatória quando clicar no botão "Tirar outra carta"
+ const url = 'https://raw.githubusercontent.com/reprograma/T9-JS-III/master/04-fetch/exercicio/tarot.json'
+ renderizaCarta(placeholderCard);
+
+ NovaCartaBtn.addEventListener("click", () => {
+    console.log('aloka');
+    fetch(url)
+        .then (response => {
+            return response.json();
+        })
+        .then ((dados) => {
+            renderizaCarta(dados[0]);
+            console.log('dados', dados)
+            const numAleatorio = Math.floor(Math.random() * dados.length);
+renderizaCarta(dados[numAleatorio]);
+        });
+});
